0dacd8129b46d9d4e339bc3f94ab6557591cb22a
Silvan Schlegel <silvanlinus.schlegel@uzh.ch>
2024-03-29 13:58:29 +0100

    added NextUI and Tailwind and modified template for it to work

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+/**
+ * Initiate nextUI:
+ */
+const {nextui} = require("@nextui-org/react");
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
+
