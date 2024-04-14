9fc937cbe430e989e8d605c68e16326ddc3c58a8
Silvan Schlegel <silvanlinus.schlegel@uzh.ch>
2024-04-14 12:44:53 +0200

    change path to background to try to make background work on deployed version.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-          'background': "url('/public/images/backgroundimage.jpg')",
+          'background': "url('/public/backgroundimage.jpg')",
       },
    },
   },
