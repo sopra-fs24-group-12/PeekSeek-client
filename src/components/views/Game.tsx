1873f8313fa3faf02272d71f41ae09acbd184739
NilsR <nils.reusch@uzh.ch>
2024-04-18 13:05:38 +0200

    Added a working version of GoogleMaps Integration, added StreetView. Coordinates and pitch/heading are saved in real-time. Also designed a new map style.

--- src/components/views/Game.tsx
+++ src/components/views/Game.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { api, handleError } from "helpers/api";
 import { Client } from '@stomp/stompjs';
+import { Library } from '@googlemaps/js-api-loader';
 
 //imports for UI
 import BaseContainer from "../ui/BaseContainer";
@@ -11,36 +12,41 @@ import GameSubmitButton from "components/ui/GameSubmitButton";
 import CreateButton from "components/ui/CreateButton";
 
 //imports for Google Maps API
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap as ReactGoogleMap, LoadScript, StreetViewPanorama , Marker } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
-  height: '400px'
+  height: '100%'
 };
+
 const API_Key = "";
 
+const libs: Library[] = ["places"];
 
-const Game = () => {
-
-
+function MyGoogleMap() {
 
   const {lobbyId} =  useParams();
   const navigate = useNavigate();
-  const [quest, setQuest] = useState("");
-  const [cityName, setCityName] = useState("");
+  const [quest, setQuest] = useState("Landmark");
+  const [cityName, setCityName] = useState("Zurich, Switzerland");
   const [roundDurationSeconds, setRoundDurationSeconds] = useState();
   const [currentRound, setCurrentRound] = useState();
   const [nrOfRounds, setNrOfRounds] = useState();
-  const [lat, setLat] = useState("");
-  const [lng, setLng] = useState("");
-  const [resLatNe, setResLatNe] = useState("");
-  const [resLngNe, setResLngNe] = useState("");
-  const [resLatSw, setResLatSw] = useState("");
-  const [resLngSw, setResLngSw] = useState("");
 
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [pitch, setPitch] = useState(0);
+  const [heading, setHeading] = useState(0);
+  const [resLatNe, setResLatNe] = useState(47.434665);
+  const [resLngNe, setResLngNe] = useState(8.625452899999999);
+  const [resLatSw, setResLatSw] = useState(47.32021839999999);
+  const [resLngSw, setResLngSw] = useState(8.448018099999999);
   const [mapCenter, setMapCenter] = useState({lat: 47.3768866, lng: 8.541694});
 
+  const [map, setMap] = useState(null);
+  const [streetView, setStreetView] = useState(null);
 
+  
   // useEffect(() => {
   //
   //   //only for dev purposes
@@ -69,6 +75,79 @@ const Game = () => {
   //   }
   //   fetchData();
   // }, [])
+  
+
+  //TODO: New file for this
+  const bareMapStyle = [
+    {
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.neighborhood",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    }
+  ];
+  
+  const handleCenterChanged = (map) => {
+    if(map){
+      // Get the new center of the map
+      const newCenter = map.getCenter();
+      
+      const tempLat = newCenter.lat();
+      const tempLng = newCenter.lng();
+
+      // Get the latitude and longitude of the new center
+      setLat(tempLat);
+      setLng(tempLng);
+      
+      // Now you can use lat and lng as needed
+      console.log("New center latitude: " + tempLat);
+      console.log("New center longitude: " + tempLng);
+    }
+  };
 
   return (
     <div className="relative min-h-screen w-screen flex flex-col items-center">
@@ -77,14 +156,56 @@ const Game = () => {
       </div>
       <div className="w-3/4 flex flex-col items-center">
         <BaseContainer size="medium" className="flex flex-col items-center mb-20">
-          <h3 className="text-xl font-bold my-4">Find a Landmark in Rome!</h3>
-          <LoadScript googleMapsApiKey={API_Key}>
-            <GoogleMap
+          <h3 className="text-xl font-bold my-4">Find a {quest} in {cityName}!</h3>
+          <LoadScript googleMapsApiKey={API_Key} libraries={libs}>
+            <ReactGoogleMap
               mapContainerStyle={containerStyle}
               center={mapCenter}
               zoom={12}
+              onLoad={(map) => {
+                const bounds = new window.google.maps.LatLngBounds(
+                  new window.google.maps.LatLng(resLatSw, resLngSw),  // Northeast
+                  new window.google.maps.LatLng(resLatNe, resLngNe)   // Southwest
+                );
+                map.fitBounds(bounds);
+                map.setOptions({ restriction: { latLngBounds: bounds, strictBounds: true } });
+                setMap(map);
+                map.set('styles', bareMapStyle);
+              }}
+              onCenterChanged={() => handleCenterChanged(map)}
             >
-            </GoogleMap>
+              <StreetViewPanorama
+                onLoad={(panorama) => {
+                  setStreetView(panorama);
+                  panorama.addListener('position_changed', () => {
+                    const newPosition = panorama.getPosition();
+                    const newLat = newPosition.lat();
+                    const newLng = newPosition.lng();
+
+                    // Update the lat and lng states
+                    setLat(newLat);
+                    setLng(newLng);
+
+                    // Now you can use newLat and newLng as needed
+                    console.log("New street view latitude: " + newLat);
+                    console.log("New street view longitude: " + newLng);
+                  });
+                  panorama.addListener('pov_changed', () => {
+                    const pov = panorama.getPov();
+                    const newPitch = pov.pitch;
+                    const newHeading = pov.heading;
+
+                    // Update the pitch and heading states
+                    setPitch(newPitch);
+                    setHeading(newHeading);
+
+                    // Now you can use newPitch and newHeading as needed
+                    console.log("New street view pitch: " + newPitch);
+                    console.log("New street view heading: " + newHeading);
+                  });
+                }}
+              />
+            </ReactGoogleMap>
           </LoadScript>
         </BaseContainer>
         <div className="w-3/4 flex justify-between px-4 absolute bottom-16">
@@ -97,4 +218,4 @@ const Game = () => {
 };
 
 
-export default Game;
+export default MyGoogleMap;
