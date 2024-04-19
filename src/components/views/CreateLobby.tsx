c73d019c29a8d1b60b2bc12e71bcc34ee37accda
Figo2003 <youssef.ehabyousseffarag@uzh.ch>
2024-04-19 10:12:10 +0200

    #73

--- src/components/views/CreateLobby.tsx
+++ src/components/views/CreateLobby.tsx
@@ -1,15 +1,72 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import BaseContainer from "../ui/BaseContainer";
-import CreateButton from "components/ui/CreateButton";
+import CreateLo from "components/ui/CreateLobbyButton";
 import BackButton from "components/ui/BackButton";
 import {Input} from "@nextui-org/react";
+import { api, handleError } from "helpers/api";
 import { useNavigate } from "react-router-dom";
-
+import Lobby from "models/Lobby";
+//daten vom user als post request 
 const CreateLobby = () => {
+  const navigate = useNavigate();
+  const [name, setLobbyname] = useState<string>(null);
+  const [username, setUsername] = useState<string>(null);
+  const [password, setLobbypassword] = useState<string>(null);
+  const handleLobbyPasswordChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    // Extract the value from the event object
+    const newPassword = event.target.value;
+    // Update the state with the new value
+    setLobbypassword(newPassword);
+  };
+  const handleLobbyAdminChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    // Extract the value from the event object
+    const newUsername = event.target.value;
+    // Update the state with the new value
+    setUsername(newUsername);
+  };
+  const handleLobbyNameChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    // Extract the value from the event object
+    const newLobbyname = event.target.value;
+    // Update the state with the new value
+    setLobbyname(newLobbyname);
+  };
+  const handleClick = () => {
+    console.log('Button clicked!');
+    doCreate();
+  };
+  const handleBackClick = () => {
+    console.log('Button clicked!');
+    navigate("/joinlobby");
+  };
+  const doCreate = async () =>{
+    try {
+
+      const requestBody = JSON.stringify({ username, name, password });
+      const response = await api.post("/lobbies", requestBody);
+     
+      //if (response.data.token){}
+      // Get the returned user and update a new object.
+      const lobby = new Lobby(response.data);
+
+
+      // Store the token into the local storage.
+      
+      //localStorage.setItem("token", lobby.token);
+      // Login successfully worked --> navigate to the route /game in the GameRouter
+      //alert("api request works")
+      navigate("/joinuser");
+    } catch (error) {
+      alert(
+        `Something went wrong during the registration, choose another username: \n${handleError(error)}`
+      );
+      navigate("/joinlobby");
+    }
+
+  }
   return (
     <div className="relative min-h-screen w-screen">
       <div className="absolute top-4 left-4">
-        <BackButton/>
+        <BackButton onClick={handleBackClick}/>
       </div>
       <div className="flex justify-center items-center h-full">
         <BaseContainer
@@ -17,14 +74,14 @@ const CreateLobby = () => {
           className="flex flex-col items-center">
           <div className="flex-row flex-wrap md:flex-nowrap mt-16 mb-16 mr-16 ml-16 gap-4">
             <text>Admin Username</text>
-            <Input className="mb-8 shadow-lg" isRequired radius={"sm"} type="username" label="required " placeholder="..."/>
+            <Input className="mb-8 shadow-lg" isRequired onChange={handleLobbyAdminChange} radius={"sm"} type="username" label="required " placeholder="..."/>
             <text>Lobby Name</text>
-            <Input className="mb-8 shadow-lg" isRequired type="name" label="required " placeholder="..." />
+            <Input className="mb-8 shadow-lg" isRequired onChange={handleLobbyNameChange}type="name" label="required " placeholder="..." />
             <text>Lobby Password</text>
-            <Input className="mb-8 shadow-lg" type="pwd" label="(optional)" placeholder="..." />
+            <Input className="mb-8 shadow-lg" onChange={handleLobbyPasswordChange} type="pwd" label="(optional)" placeholder="..." />
           </div>
           <div className="w-full flex justify-center mt-36 mb-4">
-            <CreateButton/>
+            <CreateLo onClick={handleClick}/>
           </div>
         </BaseContainer>
       </div>
