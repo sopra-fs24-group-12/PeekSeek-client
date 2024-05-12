b0d8611329ba0cf82b884c235ae4769b083cece1
asirimece <asirimece@gmail.com>
2024-05-12 10:36:19 +0200

    #153; #156 wip lobby always fit screen; modify start button

--- src/components/ui/ContentWrapper.tsx
+++ src/components/ui/ContentWrapper.tsx
@@ -3,13 +3,15 @@ import { Card, CardBody } from "@nextui-org/react";
 
 interface ContentWrapperProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const ContentWrapper: React.FC<ContentWrapperProps> = ({ children }) => {
+const ContentWrapper: React.FC<ContentWrapperProps> = ({ children, className }) => {
   return (
     <Card
       // css={{ mw: "100%", p: "$6", boxShadow: "$lg" }} // Adjust maxWidth, padding, and boxShadow as needed
       isBlurred
+      className={className}
     >
       <CardBody style={{
         display: "flex",
